50dc962e652318e479fa013bd77f55c8cd857c2b
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-06 16:28:31 +0300

    fetch creando un promesa

--- JS/fetch.js
+++ JS/fetch.js
@@ -24,8 +24,12 @@ var divuser2 = document.querySelector("#usuario");
         .then(user =>{
 
             mostarUsuario(user.data);
+            return getInfo();
 
-        }); 
+        })
+        .then(data =>{
+            console.log(data);
+        });
 
 function getUsuarios(){
     return fetch('https://reqres.in/api/users?page=2');
@@ -36,40 +40,69 @@ function getUsuario(){
 
 }
 
-    function mostarUsuario(user){
+//crear promesas
 
-        console.log(user)
-        
-
-        let nombre = document.createElement('h4');
-        let avatar = document.createElement('img');
-
-        nombre.innerHTML =  user.first_name
-
-        avatar.src = user.avatar;
-        
-        divuser2.appendChild(nombre);
-        divuser2.appendChild(avatar);
-        document.querySelector("#usuario .loading").style.display = "none";
-
-        
+function getInfo(){
+    var profesor = {
+        nombre: 'victor',
+        apellidos: 'robles',
+        url: 'victor.com'
+    }
 
 
-    };
+    // reject deenega promesas
+    //resolve respuesta correcta de proemsas 
+    return new Promise((resolve, reject) =>{
+
+     //json a srrring
+
+     var profesor_string = JSON.stringify(profesor);
+
+     if(typeof(profesor_string) != 'string')return reject("error");
+
+     return resolve(profesor_string);
+
+    })
+    
+
+}
+
+
+
+function mostarUsuario(user){
+
+    console.log(user)
+    
+
+    let nombre = document.createElement('h4');
+    let avatar = document.createElement('img');
+
+    nombre.innerHTML =  user.first_name
+
+    avatar.src = user.avatar;
+    
+    divuser2.appendChild(nombre);
+    divuser2.appendChild(avatar);
+    document.querySelector("#usuario .loading").style.display = "none";
 
     
-    function listadoUsuarios(usuarios) {
 
-        usuarios.map((data, i)=>{
 
-            let nombre = document.createElement('h2');
+};
 
-            nombre.innerHTML = i+ "." + data.first_name
-            
-            divuser.appendChild(nombre);
-            document.querySelector("#loading").style.display = "none";
 
-        });
+function listadoUsuarios(usuarios) {
 
-    };
+    usuarios.map((data, i)=>{
+
+        let nombre = document.createElement('h2');
+
+        nombre.innerHTML = i+ "." + data.first_name
+        
+        divuser.appendChild(nombre);
+        document.querySelector("#loading").style.display = "none";
+
+    });
+
+};
 });
