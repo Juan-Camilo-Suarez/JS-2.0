6de91fff67bf57135923cec55df993b9d1d95743
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-06 16:42:37 +0300

    try catch

---
+++ JS/try-catch.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var year = 2019;
+
+try{
+    alert(year);
+
+    //vuelve estring una url
+    console.log(decodeURIComponent("google.com"));
+
+}catch(error){
+    alert("hay un error en el codigo");
+}
