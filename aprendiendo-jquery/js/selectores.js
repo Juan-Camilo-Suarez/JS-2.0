abf67c2d984d4184826e92c6fa26b9964f41bf62
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-15 10:48:06 +0300

    JQuery selector by ID

---
+++ aprendiendo-jquery/js/selectores.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// siempre $ para jquery
+$(document).ready(function() { 
+    // selector ID
+    var rojo  = $("#rojo").css("background", "red")
+                        .css("color", "white");
+    
+    var amarillo = $("#amarillo").css("background", "yellow")
+    .css("color", "green"); 
+    
+    var verder = $("#verde").css("background", "green");
+    
+ });
