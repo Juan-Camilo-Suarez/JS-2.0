05c3b3ea1ebd8dbf266d2156bff7af106c094dda
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-19 17:58:16 +0300

    jquery events with hover

--- aprendiendo-jquery/js/events-02.js
+++ aprendiendo-jquery/js/events-02.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     //mouseOver y mouseOut
     var caja = $("#caja");
 
-
+/*
     caja.mouseover(function(){
         $(this).css('background', 'red');
     });
@@ -11,5 +11,17 @@ $(document).ready(function() {
     caja.mouseout(function(){
         $(this).css('background', 'green');
     });
+    */
+
+    //HOVER hace lo mismo que mouseover y mouseOut pero mas corto
+    
+    function cambiaROjo(){
+        $(this).css('background', 'red');
+    }
+    function cambiaVerde(){
+        $(this).css('background', 'green');
+    }
+    
+    caja.hover(cambiaROjo, cambiaVerde);
 
  });
