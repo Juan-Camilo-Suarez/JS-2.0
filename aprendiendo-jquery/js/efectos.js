fa8bde879c39ac88660fa0b40e62a6a689daa047
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-21 15:02:58 +0300

    jquery animaciones

---
+++ aprendiendo-jquery/js/efectos.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    //mostrar y ocultar elementos 
+
+    $('#caja').hide();
+
+    $('#mostrar').click(function() {
+        $(this).hide();
+        $('#ocultar').show();
+                        //animaciones
+        $('#caja').show('fast');
+                    //fadein()
+                    //fadeto('slow', 1)
+    });
+
+    $('#ocultar').click(function() {
+        $(this).hide();
+        $('#mostrar').show();
+                        //animaciones
+        $('#caja').hide('normal');
+                    //fadeout()
+                    //fadeto('slow', 0)
+
+    });
+
+    //todo en un solo boton
+    $('#todoenuno').click(function() {
+        $('#cajita').toggle('fast');
+        
+    });
+});
