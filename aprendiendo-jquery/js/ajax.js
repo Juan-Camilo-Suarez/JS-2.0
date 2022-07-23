42b2a83b9427aafc3e47177ff46abf4d263f7755
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-23 13:02:04 +0300

    jquery ajax method post

--- aprendiendo-jquery/js/ajax.js
+++ aprendiendo-jquery/js/ajax.js
@@ -15,5 +15,43 @@ $(document).ready(function() {
         });
 
     });
+
+    //POST
+
+    //simple option
+    /*
+    var usuario = {
+        name: "camilo",
+        job: "developer",
+
+    };
+    $.post('https://reqres.in/api/users', usuario, function(response) {
+        console.log(response);
+
+    });
+    */
+
+
+    //post of formulario html
+
+    $("#formulario").submit(function(e) {
+        //para que no me redirija
+        e.preventDefault();
+
+        var usuario = {
+            name: $('input[name="name"]').val(),
+            job: $('input[name="job"]').val(),
+    
+        };
+        console.log(usuario);
+        $.post($(this).attr("action"), usuario, function(response) {
+            console.log(response);
+    
+        }).done(function(){
+            alert("usuario corecto");
+        });
+
+        return false;
+    });
     
 });
