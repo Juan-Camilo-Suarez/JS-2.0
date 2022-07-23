54c65a40ba5e1149251841691e16ea953d56cb42
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-23 12:39:15 +0300

    jquery ajax method get

--- aprendiendo-jquery/js/ajax.js
+++ aprendiendo-jquery/js/ajax.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
 
     //LOAD
     //permite hace una peticion ajax y inscrustar el valor a una etiqueta de html
-    $("#datos").load('https://reqres.in/');
+    //$("#datos").load('https://reqres.in/');
+
+    //GET
+
+    $.get('https://reqres.in/api/users', 
+    {page : 2}, function(response){
+
+        response.data.forEach((element, index) => {
+            $("#datos").append("<p>" + element.first_name + " " + element.last_name + "</p>");
+            
+        });
+
+    });
     
 });
