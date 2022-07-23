4c67fe9e31a7d98a35fec4a18942dde1c2b6bfe9
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-23 14:05:28 +0300

    jquery-ui method resizable

--- aprendiendo-jquery/js/interface.js
+++ aprendiendo-jquery/js/interface.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     console.log('hola');
+    //mover elemento en la pg
     $(".elemento").draggable();
+
+    //redimencionar elemntos  hacerlos mas grande y pequeños 
+    $(".elemento").resizable();
 });
