480ed94090e09fc643008eadaa6d7d4fd710830e
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-23 12:28:59 +0300

    jquery ajax method load

---
+++ aprendiendo-jquery/js/ajax.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    //LOAD
+    //permite hace una peticion ajax y inscrustar el valor a una etiqueta de html
+    $("#datos").load('https://reqres.in/');
+    
+});
