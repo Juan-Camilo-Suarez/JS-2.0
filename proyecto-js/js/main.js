9017060bd7db4ff67f816a5576b7a7694b299a96
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-25 20:17:25 +0300

    list of post on a json and dinamic lict

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,7 +1,53 @@
 $(document).ready(function() {
+
+    //slider
+
     $('.bxslider').bxSlider({
         mode: 'fade',
         captions: true,
         slideWidth: 1200
       });
+
+      //post
+
+      var posts = [
+          {
+              title: "prueba de titulo 1",
+              date: "publicado el dia " + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+              content: 'Donec sed neque ac metus aliquam ultrices a a nisl. Integer id turpis quam. Sed vehicula diam augue. Aliquam lectus diam, ullamcorper eu pellentesque quis, venenatis a felis. In dapibus eleifend risus vel porttitor'
+          },
+          {
+            title: "prueba de titulo 2",
+            date: "publicado el dia " + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Donec sed neque ac metus aliquam ultrices a a nisl. Integer id turpis quam. Sed vehicula diam augue. Aliquam lectus diam, ullamcorper eu pellentesque quis, venenatis a felis. In dapibus eleifend risus vel porttitor'
+        },
+        {
+            title: "prueba de titulo 3",
+            date: "publicado el dia " + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Donec sed neque ac metus aliquam ultrices a a nisl. Integer id turpis quam. Sed vehicula diam augue. Aliquam lectus diam, ullamcorper eu pellentesque quis, venenatis a felis. In dapibus eleifend risus vel porttitor'
+        },
+        {
+            title: "prueba de titulo 4",
+            date: "publicado el dia " + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Donec sed neque ac metus aliquam ultrices a a nisl. Integer id turpis quam. Sed vehicula diam augue. Aliquam lectus diam, ullamcorper eu pellentesque quis, venenatis a felis. In dapibus eleifend risus vel porttitor'
+        },
+      ]
+
+      posts.forEach((item,index)=>{
+          var post = `
+
+            <article class="post">
+            <h2> ${item.title}</h2>
+            <span class="date"> ${item.date}</span>
+            <p>
+            ${item.content}
+            </p>
+            <a href="#" class="button-more"> Leer mas</a>
+            </article>
+          
+          `;
+          $("#posts").append(post);
+        
+      })
+
 });
