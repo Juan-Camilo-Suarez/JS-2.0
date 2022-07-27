c93e127aaa13701200f23e301e2e4e69c916ed76
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-27 15:15:13 +0300

    simple ts

---
+++ poo-y-javascript/js/tipos-de-datos.js
@@ -0,0 +1,13 @@
+//string
+var cadena = "camilo";
+console.log(cadena);
+//int
+var numero = 25;
+//boolean
+var verdadero_falso = true;
+//datos any
+var cualqueira = "hola";
+//arrays
+var leguages = ['java', 'ts'];
+var years = [2009, 485, 43];
+console.log(cadena, numero, verdadero_falso, cualqueira, leguages, years);
---
+++ poo-y-javascript/js/tipos-de-datos.ts
@@ -0,0 +1,23 @@
+//string
+
+let cadena: string = "camilo";
+console.log(cadena);
+
+
+//int
+let numero: number = 25;
+
+//boolean
+let verdadero_falso: boolean = true;
+
+//datos any
+
+let cualqueira: any = "hola"
+
+
+//arrays
+var leguages : Array<string> = ['java', 'ts'] ;
+
+var years: number[] = [2009,485,43];
+
+console.log(cadena, numero, verdadero_falso, cualqueira, leguages, years);
