5a9d398b51e927cf78c30ab3d88e971bf78d497e
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-27 11:57:05 +0300

    identification Local Storage

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -64,11 +64,11 @@ $(document).ready(function() {
       $('#to-red').click(function(){
 
         theme.attr('href', "css/red.css");
-      });
+    });
 
 
       //scroll arriba de la web
-      
+
       $(".subir").click(function(e){
           e.preventDefault();
 
@@ -78,4 +78,30 @@ $(document).ready(function() {
             return false;
       });
 
+      //login falso
+      $("#login form").submit(function(e){
+          var form_name = $('#form_name').val();
+          localStorage.setItem('form_name', form_name);
+      });
+
+      var form_name = localStorage.getItem('form_name');
+      
+      if (form_name != null && form_name != undefined) {
+
+        var about_parrafo =$("#about p")
+        about_parrafo.html("<br><strong> bienvenido, " + form_name + "</strong>");
+        about_parrafo.append("<a href='#' id = 'logout'> logout </a>");
+      
+        $("#login").hide();
+
+        $('#logout').click(function(){
+            localStorage.clear();
+            window. location.reload();
+        });
+    
+
+      }
+
+        
+    
 });
